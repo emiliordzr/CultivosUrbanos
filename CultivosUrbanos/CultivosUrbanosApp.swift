cfb3af535a94b758f88f961ad4720fdb18116b8a
Fer Ahuatzin <64028335+FerAhuatzin@users.noreply.github.com>
2024-04-11 12:26:21 -0600

    Initial Commit

---
+++ CultivosUrbanos/CultivosUrbanosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CultivosUrbanosApp.swift
+//  CultivosUrbanos
+//
+//  Created by iOS Lab on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CultivosUrbanosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
